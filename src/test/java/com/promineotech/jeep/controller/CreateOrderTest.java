395748514eb7c577ba20a4a2471a6ce5337b02f4
bman354 <bpink354@gmail.com>
2023-02-04 17:27:33 -0500

    created order controller and service, full implementation of fetch jeeps

--- src/test/java/com/promineotech/jeep/controller/CreateOrderTest.java
+++ src/test/java/com/promineotech/jeep/controller/CreateOrderTest.java
@@ -1,5 +1,6 @@
 package com.promineotech.jeep.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,62 +17,58 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 
-import com.mysql.cj.x.protobuf.MysqlxCrud.Order;
 import com.promineotech.jeep.controller.support.CreateOrderTestSupport;
 import com.promineotech.jeep.entity.JeepModel;
-
+import com.promineotech.jeep.entity.Order;
 import lombok.Getter;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-@Sql(scripts = { "classpath:flyway/migrations/V1.0__Jeep_Schema.sql",
-		"classpath:flyway/migrations/V1.1__Jeep_Data.sql" }, config = @SqlConfig(encoding = "utf-8"))
+@Sql(scripts = {"classpath:flyway/migrations/V1.0__Jeep_Schema.sql",
+    "classpath:flyway/migrations/V1.1__Jeep_Data.sql"}, config = @SqlConfig(encoding = "utf-8"))
 
 class CreateOrderTest extends CreateOrderTestSupport {
-	
-	@LocalServerPort
-	int serverPort;
-	
-	@Autowired
-	@Getter
-	TestRestTemplate restTemplate;
-	
-	
-	@Test
-	void testCreateOrderReturnsSuccess201() {
-		
-		String body = createOrderBody();
-		String uri = String.format("http://localhost:%d/orders", serverPort);
-		
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		
-		HttpEntity<String> bodyEntity = new HttpEntity<>(body, headers);
-		
-		ResponseEntity<Order> response = restTemplate.exchange(uri, HttpMethod.POST, bodyEntity, Order.class);
-		
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-		assertThat(response.getBody()).isNotNull();
 
-		Order order = response.getBody();
-		
-		assertThat(order.getCustomer().getCustomerId()).isEqualTo("MORISON_LINA");
-		assertThat(order.getModel().getModelId()).isEqualTo(JeepModel.WRANGLER);
-		assertThat(order.getModel().getTrimLevel()).isEqualTo("Sport Altitude");
-		assertThat(order.getModel().getNumDoors()).isEqualTo(4);
-		assertThat(order.getColor().getColorId()).isEqualTo("EXT_NACHO");
-		assertThat(order.getEngine().getEngineId()).isEqualTo("2_0_TURBO");
-		assertThat(order.getTire().getTireId()).isEqualTo("35_TOYO");
-		assertThat(order.getOptions()).hasSize(6);
+  @LocalServerPort
+  int serverPort;
 
-		
-		
-		System.out.println(body);
-	}
+  @Autowired
+  @Getter
+  TestRestTemplate restTemplate;
 
-	protected String createOrderBody() {
 
-		//@formatter:off
+  @Test
+  void testCreateOrderReturnsSuccess201() {
+
+    String body = createOrderBody();
+    String uri = String.format("http://localhost:%d/orders", serverPort);
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+
+    HttpEntity<String> bodyEntity = new HttpEntity<>(body, headers);
+
+    ResponseEntity<Order> response =
+        restTemplate.exchange(uri, HttpMethod.POST, bodyEntity, Order.class);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    assertThat(response.getBody()).isNotNull();
+
+    Order order = response.getBody();
+
+    assertThat(order.getCustomer().getCustomerId()).isEqualTo("MORISON_LINA");
+    assertThat(order.getModel().getModelId()).isEqualTo(JeepModel.WRANGLER);
+    assertThat(order.getModel().getTrimLevel()).isEqualTo("Sport Altitude");
+    assertThat(order.getModel().getNumDoors()).isEqualTo(4);
+    assertThat(order.getColor().getColorId()).isEqualTo("EXT_NACHO");
+    assertThat(order.getEngine().getEngineId()).isEqualTo("2_0_TURBO");
+    assertThat(order.getTire().getTireId()).isEqualTo("35_TOYO");
+    assertThat(order.getOptions()).hasSize(6);
+  }
+
+  protected String createOrderBody() {
+
+    //@formatter:off
 		String body =  "{"
 			+"\"customer\":\"MORISON_LINA\","
 			+"\"model\":\"WRANGLER\","
@@ -91,5 +88,5 @@ class CreateOrderTest extends CreateOrderTestSupport {
 			+"}";
 		return body;
 		//@formatter:on
-	}
+  }
 }
